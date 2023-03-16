f30dc749df8c949b86be9f511da95ce4832928ea
Anas TAHER <taheranas24@gmail.com>
2023-03-17 00:03:52 +0100

    Task 8 Unix fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar,2015-10-19";
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	size_t len = strlen(message);
 
