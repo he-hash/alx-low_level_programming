07f822a024e7c3c8771011c26e5510b0c8f9233c
Anas TAHER <taheranas24@gmail.com>
2023-03-17 21:54:56 +0100

    Task 8 fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,13 @@
 
 int main(void)
 {
-	for (char c = '0'; c <= '9'; c++)
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
 		putchar(c);
-	for (char c = 'a'; c <= 'f'; c++)
+	for (c = 'a'; c <= 'f'; c++)
 		putchar(c);
 	putchar('\n');
+
+	return (0);
 }
