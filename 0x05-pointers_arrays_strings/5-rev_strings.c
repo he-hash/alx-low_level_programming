28948b7ed7499391e7c15fab5434569bf81e21b5
Anas TAHER <taheranas24@gmail.com>
2023-03-28 11:56:32 +0200

    Task 5 commit

---
+++ 0x05-pointers_arrays_strings/5-rev_strings.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	char temp;
+
+	while (s[i] != '\0')
+		i++;
+	i--;
+	while (i > j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i--;
+		j++;
+	}
+}
